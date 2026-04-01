6c709a0aff7728eb35374c9e37e6278659a146e3
sesmoi123 <cherifdhaouadi947@gmail.com>
2026-04-01 18:49:08 +0200

    Update API_URL to new endpoint00

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"http://185.46.112.196:3551"
+#define API_URL @"http://51.210.109.115:3551"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
